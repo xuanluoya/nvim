da6d2fe7f7fc3d6348fefe8a25336ab17fbe5668
Aislin <72391548+xuanluoya@users.noreply.github.com>
2025-08-23 20:23:24 +0800

    first baka

---
+++ lua/options.lua
@@ -0,0 +1,10 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+
+-- o.cursorlineopt ='both' -- to enable cursorline!
+
+o.number = true
+o.relativenumber = true
---
+++ lua/plugins/init.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "mg979/vim-visual-multi",
+    branch = "master",
+    event = "VeryLazy",
+    init = function()
+      vim.g.VM_maps = {
+        ["Select Cursor Down"] = "<C-j>",
+        ["Select Cursor Up"]   = "<C-k>",
+        ["Find Under"]         = "<C-d>",
+      }
+    end
+  },
+
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
